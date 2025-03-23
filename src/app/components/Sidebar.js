a58c2e6e8cfa03a6daf32ee6cf18d04cb46a5f2e
OscarMinjarez <minjarez.dev@gmail.com>
2025-03-22 21:40:20 -0700

    Sidebar parcialmente finalizado

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -25,13 +25,40 @@ export default function Sidebar() {
         return null
     };
     return (
-        <nav className="absolute block h-full w-full sm:w-[300px] lg:w-[400px] bg-[#e9eaed]">
-            <div className="p-4">
-                <ul>
-                    <Item label={"Dashboard"} path="dashboard" icon="fa-solid fa-house"/>
-                    <Item label={"Calendario"} path={"calendar"} icon="fa-solid fa-calendar"/>  
-                    <Item label={"Mi día"} path={"myday"} icon="fa-solid fa-sun"/>
-                </ul>
+        <nav className="absolute flex flex-col items-center h-full w-full sm:w-[300px] lg:w-[400px] bg-[#c9d6ff]">
+            <div className="w-full h-full flex flex-col">
+                <div className="w-full p-4">
+                    <input
+                        id="search"
+                        placeholder="Buscar"
+                        type="text"
+                        className="block min-w-0 grow bg-white p-1 rounded-[8px] w-full"/>
+                </div>
+
+                <div className="p-4 w-full">
+                    <ul>
+                        <Item label={"Dashboard"} path="dashboard" icon="fa-solid fa-house"/>
+                        <Item label={"Calendario"} path={"calendar"} icon="fa-solid fa-calendar"/>  
+                        <Item label={"Mi día"} path={"myday"} icon="fa-solid fa-sun"/>
+                    </ul>
+                </div>
+
+                <div className="my-2 h-[2px] bg-[rgba(100,103,209,0.5)] w-[95%]"></div>
+
+                <div className="p-4 w-full">
+                    <ul>
+                        <Item label={"Escuela"} path="escuela" icon="fa-solid fa-bars"/>
+                        <Item label={"Cocina"} path={"cocina"} icon="fa-solid fa-bars"/>  
+                        <Item label={"Personal"} path={"personal"} icon="fa-solid fa-bars"/>
+                        <Item label={"Social"} path={"social"} icon="fa-solid fa-bars"/>
+                    </ul>
+                </div>
+
+                <div className="p-4 w-full mt-auto">
+                    <ul>
+                        <Item label={"Nueva lista"} path="lista" icon="fa-solid fa-plus"/>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
