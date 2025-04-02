772c06cdd7ad33674652dd9ff9a9edd1fb570b3c
JLMadero <madero26jose08@gmail.com>
2025-04-01 23:02:21 -0700

    modificacion en route
    falta aclarar bien el from del object taskService

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,22 +1,28 @@
 import { NextResponse } from "next/server";
+import { taskService } from "./taskservice";
+
 
 export async function GET() {
     return Response.json({message: "hola"});
 }
 
-export async function POST(req) {
+export async function POST(req: NextResponse) {
     try {
         const body = await req.json(); // Convertimos el cuerpo de la petición a JSON
-        const { descripcion, estado, asignado, titulo, fecha } = body; // Extraemos los datos
+        const { descripcion, estado, titulo, fechaCreacion, fechaLimite } = body; // Extraemos los datos
 
         // Validamos que todos los campos existen
-        if (!descripcion || !estado || !asignado || !titulo || !fecha) {
+        if (!descripcion || !estado || !fechaCreacion || !titulo || !fechaLimite) {
             return NextResponse.json({ error: "Todos los campos son requeridos" }, { status: 400 });
         }
 
+        
+        // Agregamos la tarea y obtenemos el ID generado
+        const taskId = taskService.addTask(body);
+
         return NextResponse.json({
             message: "Tarea recibida",
-            tarea: { descripcion, estado, asignado, titulo, fecha },
+            tareaId: taskId,
         });
     } catch (error) {
         return NextResponse.json({ error: "Error procesando la solicitud" }, { status: 500 });
