057633c98a3ad150e4ea5974369f9120ce5cfb98
OscarMinjarez <minjarez.dev@gmail.com>
2025-04-02 18:09:31 -0700

    Diseño de opciones de la tarea

--- src/app/components/TaskItem.js
+++ src/app/components/TaskItem.js
@@ -1,8 +1,20 @@
 "use client";
 
+function ListOptions({ options }) {
+    return (
+        <div>
+            <select className="bg-red-200 border-red-400 text-black py-1 px-2 rounded-[6px]">
+                {options.map((option) => {
+                    return <option key={option}>{option}</option>
+                })}
+            </select>
+        </div>
+    );
+}
+
 export default function TaskItem() {
     return (
-        <div className="bg-white w-full h-auto flex justify-between">
+        <div className="bg-white w-full h-auto flex justify-between items-center px-5 py-2 rounded-[12px] my-2">
             <div className="flex flex-col">
                 <h1 className="font-bold">Diseñar Frontend</h1>
 
@@ -12,22 +24,15 @@ export default function TaskItem() {
                 </div>
             </div>
 
-            <div className="flex">
+            <div className="flex gap-x-3">
                 <div>
-                    <select>
-                        <option>Escuela</option>
-                        <option>Cocina</option>
-                        <option>Personal</option>
-                        <option>Social</option>
-                    </select>
+                    <ListOptions
+                        options={["Escuela", "Cocina", "Personal", "Social"]}/>
                 </div>
 
                 <div className="">
-                    <select>
-                        <option>En progreso</option>
-                        <option>Pendiente</option>
-                        <option>Completada</option>
-                    </select>
+                    <ListOptions
+                        options={["En progreso", "Completado", "Pendiente"]}/>
                 </div>
             </div>
         </div>
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -6,7 +6,11 @@ export default function Dashboard() {
     return (
         <div className="dashboard-container bg-[#eef2ff] flex">
             <Sidebar />
-            <div className="flex flex-col w-full items-center">
+            <div className="flex flex-col w-full items-center p-4">
+                <TaskItem />
+                <TaskItem />
+                <TaskItem />
+                <TaskItem />
                 <TaskItem />
             </div>
         </div>
