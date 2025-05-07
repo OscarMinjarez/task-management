ed1e2824940b50df9939262c52f035dd4d95ffea
Magda Ramirez <maparaes03@gmail.com>
2025-05-07 16:11:14 -0700

    Resolución de conflictos en la configuración de la base de datos

--- src/backend/config-database.ts
+++ src/backend/config-database.ts
@@ -12,7 +12,11 @@ export async function getConnection(): Promise<DataSource> {
             host: "localhost",
             port: 3306,
             username: "root",
+<<<<<<< Updated upstream
             password: "itson",
+=======
+            password: "1234",
+>>>>>>> Stashed changes
             database: "tasks_management_db",
             entities: [
                 User, Task, List
